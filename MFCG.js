2060bb0f9221eff008dc50817dac452eb51385bb
tulefat <tolefat1989@gmail.com>
2025-09-29 20:40:05 +0300

    create match card function for the game logic by using if statement and other related function to reset the click array and return the card if not match

--- MFCG.js
+++ MFCG.js
@@ -1,21 +1,22 @@
 ////////////////////////////////
 // Global Variables Here
 
-let card = ["https://i.pinimg.com/1200x/f8/7d/1f/f87d1fd4c090d46a87590f4a56a80502.jpg","https://i.pinimg.com/1200x/f8/7d/1f/f87d1fd4c090d46a87590f4a56a80502.jpg","https://i.pinimg.com/736x/eb/18/c7/eb18c707eecc272af360935ffb57ebf8.jpg","https://i.pinimg.com/736x/eb/18/c7/eb18c707eecc272af360935ffb57ebf8.jpg","https://i.pinimg.com/736x/c1/84/94/c18494c7eee49ba76d4c429687473532.jpg","https://i.pinimg.com/736x/c1/84/94/c18494c7eee49ba76d4c429687473532.jpg","https://i.pinimg.com/736x/d0/49/9f/d0499f16a0bb36301c2dc92e38834932.jpg","https://i.pinimg.com/736x/d0/49/9f/d0499f16a0bb36301c2dc92e38834932.jpg","https://i.pinimg.com/736x/33/48/e9/3348e9ba08a47c18d41203174152d1ea.jpg"]
+let cards = ["https://i.pinimg.com/1200x/f8/7d/1f/f87d1fd4c090d46a87590f4a56a80502.jpg","https://i.pinimg.com/1200x/f8/7d/1f/f87d1fd4c090d46a87590f4a56a80502.jpg","https://i.pinimg.com/736x/eb/18/c7/eb18c707eecc272af360935ffb57ebf8.jpg","https://i.pinimg.com/736x/eb/18/c7/eb18c707eecc272af360935ffb57ebf8.jpg","https://i.pinimg.com/736x/c1/84/94/c18494c7eee49ba76d4c429687473532.jpg","https://i.pinimg.com/736x/c1/84/94/c18494c7eee49ba76d4c429687473532.jpg","https://i.pinimg.com/736x/d0/49/9f/d0499f16a0bb36301c2dc92e38834932.jpg","https://i.pinimg.com/736x/d0/49/9f/d0499f16a0bb36301c2dc92e38834932.jpg","https://i.pinimg.com/736x/33/48/e9/3348e9ba08a47c18d41203174152d1ea.jpg"]
 let score = 0
-let tries = 0
-let newArray=[]
-const cards= document.querySelectorAll(".cards")
+let tries = 3
+let checkClicks=[]
+const card= document.querySelectorAll(".card img")
 const messageEl= document.querySelector("#message")
 const resetBtnEl= document.querySelector("#reset")
 const scoreEl=document.querySelector("#score")
+
 
 ////////////////////////////////
 // Functions For Game Logic Here
 const init=()=>
 {
 score= 0;
-tries= 0;
+tries= 3;
 render()
 }
 
@@ -37,4 +38,19 @@ const updateMessage=()=>{
   messageEl.textContent=`score: ${score} tries: ${tries}`
 }
 
+const newClick=()=>{
+  checkClicks=[]
+}
+
+const reset=(firstClick,secondClick) =>{
+  card[firstClick,secondClick]=card[cards,cards]
+}
+
+const matchCard = () => {
+  let checkClicks= [firstClick,secondClick]
+   if (card[firstClick]===card[secondClick]){score++;updateMessage();newClick()}
+   ;if(score===4){messageEl.textContent="You Win :)"}
+  else if(tries>=1){tries--;updateMessage();newClick();reset(firstClick,secondClick)}
+  else {tries=0 ;updateMessage();messageEl.textContent="You lose !"}
+}
 
