bc95d1e106eb86826b64015378575f4476d0beec
tulefat <tolefat1989@gmail.com>
2025-09-30 18:36:15 +0300

    initiate event listener to each HTML img

--- MFCG.js
+++ MFCG.js
@@ -5,7 +5,7 @@ let cards = ["https://i.pinimg.com/1200x/f8/7d/1f/f87d1fd4c090d46a87590f4a56a805
 let score = 0
 let tries = 3
 let checkClicks=[]
-const card= document.querySelectorAll(".card img")
+const cardEls= document.querySelectorAll(".card img")
 const messageEl= document.querySelector("#message")
 const resetBtnEl= document.querySelector("#reset")
 const scoreEl=document.querySelector("#score")
@@ -17,6 +17,7 @@ const init=()=>
 {
 score= 0;
 tries= 3;
+checkClicks=[];
 render()
 }
 
@@ -27,11 +28,11 @@ updatecards();updateMessage()
 
 const updatecards=()=>
 {
-card.forEach((eachcard,index)=>{
+cards.forEach((eachcard,index)=>{
 let random= Math.floor(Math.random()* card.length);
-let oldCard=card[index];
-card[index]=card[random];
-card[random]=oldCard
+let oldCard=cards[index];
+cards[index]=cards[random];
+cards[random]=oldCard
 })
 }
 const updateMessage=()=>{
@@ -43,14 +44,21 @@ const newClick=()=>{
 }
 
 const reset=(firstClick,secondClick) =>{
-  card[firstClick,secondClick]=card[cards,cards]
+  cardEls[firstClick].src="https://i.pinimg.com/1200x/b9/5b/69/b95b69dba492dad85f098702c032c615.jpg";cardEls[secondClick].src="https://i.pinimg.com/1200x/b9/5b/69/b95b69dba492dad85f098702c032c615.jpg"
 }
 
-const matchCard = () => {
-  let checkClicks= [firstClick,secondClick]
-   if (card[firstClick]===card[secondClick]){score++;updateMessage();newClick()}
-   ;if(score===4){messageEl.textContent="You Win :)"}
-  else if(tries>=1){tries--;updateMessage();newClick();reset(firstClick,secondClick)}
-  else {tries=0 ;updateMessage();messageEl.textContent="You lose !"}
+const matchCard = (firstClick,secondClick) => {
+checkClicks= [firstClick,secondClick]
+if (cards[firstClick]===cards[secondClick]){score++;updateMessage();newClick()}
+;if(score===4){messageEl.textContent="You Win :)"}
+else if(tries>=1){tries--;updateMessage();newClick();reset(firstClick,secondClick)}
+else {tries=0 ;updateMessage();messageEl.textContent="You lose !"}
 }
+
+cardEls.forEach((singleCard,index)=>{
+singleCard.addEventListener("click",()=>{
+singleCard=cards[index];
+checkClicks.push(index);
+})}
+)
 
